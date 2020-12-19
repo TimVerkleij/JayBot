97c62bc33ffb4e291bc2b0636a735ea831a32ac0
TimVerkleij <tim.verkleij@student.hu.nl>
2020-12-19 19:16:40 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,44 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+
+
+client.once('ready', () => {
+ console.log('Ready!');
+});
+
+
+client.login
+('Nzg5Njc1MDQyNjYxMDcyOTI2.X91gMQ.wXhQYW5bEqLNJnbXUvDLRzt1db0');
+
+client.on('ready', () => {
+  console.log("Active")
+
+  setInterval(() => {
+    const statuses = [
+      'Follow Jay on twitch',
+      'JayTheCoug'
+    ]
+
+    const status = statuses[Math.floor(Math.random() * statuses.length)]
+    client.user.setActivity(status, { type: "PLAYING" })
+  }, 5000)
+})
+
+
+
+
+
+client.on('message', message => {
+  if(message.content === '$twitch'){
+  message.channel.send('Jays twitch is JayTheCoug');
+  }else if(message.content === '$info'){
+    message.channel.send('This server is for Jays community!');
+  }else if(message.content === '$help'){
+    message.channel.send('IF you need help or have any questions ping or dm the mods!');
+  }else if(message.content === 'hi'){
+    message.channel.send('Hello!');
+  }else if(message.content === 'prefix'){
+    message.channel.send('our prefix for all of our commands is $');
+  }
+  });
